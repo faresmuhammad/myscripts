5d1ab5efd547d1233f8592906adeb3373fa5a2d9
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2024-08-04 20:40:56 +0300

    Modify fresh-install.sh

--- bash/fresh-install.sh
+++ bash/fresh-install.sh
@@ -22,7 +22,7 @@ echo "-------------------------------------------------------"
 
 echo "Installing OneDrive..."
 
-sudo dnf install onedrive
+sudo dnf install onedrive -y
 
 echo "Done OneDrive"
 echo "-------------------------------------------------------"
@@ -32,13 +32,13 @@ echo "Installing VSCode..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-sudo dnf install code
+sudo dnf install code -y
 
 echo "Done VSCode"
 echo "-------------------------------------------------------"
 
 echo "Installing GitHub CLI..."
-sudo dnf install gh
+sudo dnf install gh -y
 echo "Done GitHub CLI"
 echo "-------------------------------------------------------"
 
@@ -46,13 +46,19 @@ echo "Installing Composer..."
 sudo dnf install php-cli unzip curl -y
 
 cd ~
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 
-HASH=`curl -sS https://composer.github.io/installer.sig`
+if [ "$EXPECTED_SIGNATURE" != "$SIGNATURE" ]
+then
+    echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
 
-php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-
-sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
 
 echo "Done Composer"
 echo "-------------------------------------------------------"
