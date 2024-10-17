27948bc6ce89ca251d3fdca54d0fe5a50555532c
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2024-10-17 11:01:20 +0300

    Add some notes on work setup script

--- work-setup.sh
+++ work-setup.sh
@@ -5,7 +5,20 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
+read -p "Did you install XAMPP and set the configuration?" xampp
+if [ $xampp == 'y' ]; then
+    echo "Installing composer"
+fi
+# run composer command after installation to verify it's installed
+
+# add other extension or edit the file after installation to add the unrecognized extensions
+sudo dnf install php-zip php-gd php-xml -y
+
+echo "Installing GitHub cli and GitLab cli..."
 # install xampp
 # install composer
-# install php extensions curl gd mbstring zip xml soap mongodb
+# install php extensions [install on device and enable in php.ini] curl gd fileinfo intl exif mysqli openssl pdo_mysql mbstring zip xml soap mongodb sodium xsl
 # install postman
+# install warp terminal
+# install gh glab
+# install fzf
