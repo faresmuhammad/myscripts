c333a142eda658f7734357267e71f1575eaf3c74
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2024-10-21 00:05:38 +0300

    Some modifications on work setup ubuntu and bash aliases

--- my-bashrc-aliases.sh
+++ my-bashrc-aliases.sh
@@ -1,5 +1,8 @@
 #This file isn't executable, it is just a reference to copy them into a new system
 
+alias in="sudo dnf install"
+alias dser="dnf search"
+
 alias pa="php artisan $1"
 alias startweb="sudo /opt/lampp/lampp startapache && sudo /opt/lampp/lampp startmysql"
 alias stopweb="sudo /opt/lampp/lampp stopapache && sudo /opt/lampp/lampp stopmysql"
@@ -9,6 +12,10 @@ alias gaa="git add ."
 alias ga="git add"
 alias gc="git commit -m"
 alias gaac="git add . && git commit -m"
+alias gck="git checkout"
+alias gnb="git checkout -b"
+alias gb="git branch"
+
 
 # Pull the latest changes from a remote branch
 # (current by default, or you can speficy which specific branch you'd like to pull from)
@@ -33,5 +40,13 @@ function gpush () {
 }
 
 
+function cursor(){
+  (nohup /usr/local/bin/cursor.AppImage "$@" > /dev/null 2>&1 &)
+}
+
 alias lvi="composer install && npm install"
 alias lvcl="cp .env.example .env && lvi && pa key:generate"
+
+alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+alias fp="fzf --preview 'cat {} | bat --style=numbers --color=always'"
--- work-setup-ubuntu.sh
+++ work-setup-ubuntu.sh
@@ -5,11 +5,17 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
+sudo apt-add-repository ppa:ondrej/php
+sudo apt-add-repository main
+sudo apt-add-repository restricted
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+
+sudo apt-get install bat php8.1-cli unzip curl php8.1-zip php8.1-gd php8.1-xml php8.1-mongodb php8.1-soap -y
 
 read -p "Did you install XAMPP and set the configuration? " xampp
 if [ $xampp == 'y' ]; then
     echo "Installing php extensions...";echo ""
-    sudo apt install php-cli unzip curl php-zip php-gd php-xml php-mongodb php-soap -y
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
     SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
     EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
@@ -24,19 +30,24 @@ if [ $xampp == 'y' ]; then
     php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
     rm composer-setup.php
     echo "done"
+    composer -V
 fi
 # run composer command after installation to verify it's installed
 
 # add other extension or edit the file after installation to add the unrecognized extensions
 
-echo "Installing Some Essential Packages gh, glab, fzf"
+echo "Installing Some Essential Packages gh, fzf"
 sudo apt install git gh glab fzf -y
+echo ""
 
-read -p "Did you configure git, gh and glab?" git
-
-if [ $git == 'y' ];then
-    echo "Well Done!"
-    echo "Next steps -> install warp terminal, postman, phpstorm, vscode, tableplus, dbeaver, AppImageLauncher, clickup, zoom, mailspring and configure bitwarden"
-    echo ""
-    echo "then -> copy bash aliases from faresmuhammad/myscripts repo"
-fi
+echo "Well Done!"
+echo "Configure Bitwarden";echo "";
+echo "Install Gitlab Cli -> https://about.gitlab.com/install/#ubuntu";echo "";
+echo "Install Warp Terminal -> https://www.warp.dev/linux-terminal";echo "";
+echo "Install Warp themes -> https://docs.warp.dev/appearance/custom-themes";echo "";
+echo "Install Postman -> https://www.postman.com/downloads/";echo "";
+echo "Install JetBrains ToolBox -> https://www.jetbrains.com/toolbox-app/";echo "";
+echo "Install Table Plus -> https://tableplus.com/download";echo "";
+echo "Next steps -> install warp terminal, postman, phpstorm, vscode, tableplus, dbeaver, AppImageLauncher, clickup, zoom, mailspring and configure bitwarden"
+echo ""
+echo "then -> copy bash aliases from faresmuhammad/myscripts repo"
