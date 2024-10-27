b58907d6115266657999bfc6aa1a496d10bc022f
Fares Muhammad <fares.muhammad@adamtravel.net>
2024-10-27 11:41:39 +0300

    Add xsel aliases to bashrc

--- my-bashrc-aliases.sh
+++ my-bashrc-aliases.sh
@@ -50,3 +50,6 @@ alias lvcl="cp .env.example .env && lvi && pa key:generate"
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
 
 alias fp="fzf --preview 'cat {} | bat --style=numbers --color=always'"
+
+alias pbcopy="xsel --clipboard"
+alias pbpaste="xsel --clipboard --output"
